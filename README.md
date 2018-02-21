d9cd9b4a8a5c59a4f4cb172bc72e1a59f45e8d10
CosimoLovascio <cosimolovascio@hotmail.it>
2018-02-21 17:03:03 +0100

    Added calculate field in configuration file

--- README.md
+++ README.md
@@ -7,3 +7,45 @@ Crowd Pulse Wikipedia tag categorizer.
 
 The `wikipedia` plugin tries to associate Wikipedia categories to non-stop-word tags 
 in the Messages flowing through the stream.
+
+You can specify the configuration option "calculate" with one of the following values:
+- all: categorize all tags coming from the stream;
+- new: categorize the tags with no categories (property is null or array is empty);
+
+Example of usage:
+```json
+{
+  "process": {
+    "name": "wikipedia-tester",
+    "logs": "/opt/crowd-pulse/logs"
+  },
+  "nodes": {
+    "fetch": {
+      "plugin": "message-fetch",
+      "config": {
+        "db": "test-wikipedia"
+      }
+    },
+    "wiki": {
+      "plugin": "wikipedia",
+      "config": {
+        "calculate": "new"
+      }
+    },
+    "persistance": {
+      "plugin": "message-persist",
+      "config": {
+        "db": "test-wikipedia"
+      }
+    }
+  },
+  "edges": {
+    "fetch": [
+      "wiki"
+    ],
+    "wiki": [
+      "persistance"
+    ]
+  }
+}
+```
--- src/main/java/com/github/frapontillo/categorize/wikipedia/WikipediaTagCategorizer.java
+++ src/main/java/com/github/frapontillo/categorize/wikipedia/WikipediaTagCategorizer.java
@@ -6,12 +6,14 @@ import com.github.frapontillo.pulse.crowd.data.entity.Category;
 import com.github.frapontillo.pulse.crowd.data.entity.Message;
 import com.github.frapontillo.pulse.crowd.data.entity.Tag;
 import com.github.frapontillo.pulse.spi.IPlugin;
-import com.github.frapontillo.pulse.spi.VoidConfig;
+import com.github.frapontillo.pulse.spi.IPluginConfig;
+import com.github.frapontillo.pulse.spi.PluginConfigHelper;
 import com.github.frapontillo.pulse.util.PulseLogger;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.github.frapontillo.categorize.wikipedia.rest.WikipediaResponseDeserializer;
 import com.github.frapontillo.categorize.wikipedia.rest.WikipediaService;
+import com.google.gson.JsonElement;
 import org.apache.logging.log4j.Logger;
 import retrofit.RestAdapter;
 import retrofit.RetrofitError;
@@ -25,7 +27,7 @@ import java.util.Map;
 /**
  * @author Francesco Pontillo
  */
-public class WikipediaTagCategorizer extends IPlugin<Message, Message, VoidConfig> {
+public class WikipediaTagCategorizer extends IPlugin<Message, Message, WikipediaTagCategorizer.WikipediaTagCategorizerConfig> {
     public static final String PLUGIN_NAME = "wikipedia";
     private static final String WIKIPEDIA_ENDPOINT_1 = "http://";
     private static final String WIKIPEDIA_ENDPOINT_2 = ".wikipedia.org/w";
@@ -42,15 +44,33 @@ public class WikipediaTagCategorizer extends IPlugin<Message, Message, VoidConfi
         return PLUGIN_NAME;
     }
 
-    @Override public VoidConfig getNewParameter() {
-        return new VoidConfig();
+    @Override public WikipediaTagCategorizerConfig getNewParameter() {
+        return new WikipediaTagCategorizerConfig();
     }
 
-    @Override public Observable.Operator<Message, Message> getOperator(VoidConfig parameters) {
+    @Override public Observable.Operator<Message, Message> getOperator(WikipediaTagCategorizerConfig parameters) {
         return new ITagCategorizerOperator(this) {
             @Override public List<Category> getCategories(Tag tag) {
                 WikipediaService wikipediaService = getService(tag.getLanguage());
                 try {
+
+                    if (parameters != null) {
+                        switch (parameters.getCalculate()) {
+                            case WikipediaTagCategorizerConfig.ALL:
+                                return wikipediaService.tag(tag.getText()).getCategories();
+
+                            case WikipediaTagCategorizerConfig.NEW:
+                                if (tag.getCategories() == null || tag.getCategories().size() == 0) {
+                                    return wikipediaService.tag(tag.getText()).getCategories();
+                                } else {
+                                    logger.info("Tag skipped (categories already exist)");
+                                    return null;
+                                }
+                            default:
+                                return wikipediaService.tag(tag.getText()).getCategories();
+                        }
+                    }
+
                     return wikipediaService.tag(tag.getText()).getCategories();
                 } catch (RetrofitError error) {
                     logger.error(error);
@@ -83,5 +103,33 @@ public class WikipediaTagCategorizer extends IPlugin<Message, Message, VoidConfi
             wikipediaServiceMap.put(language, wikipediaService);
         }
         return wikipediaService;
+    }
+
+    public class WikipediaTagCategorizerConfig implements IPluginConfig<WikipediaTagCategorizerConfig> {
+
+        /**
+         * Categorize of all tags coming from the stream.
+         */
+        public static final String ALL = "all";
+
+        /**
+         * Categorize the tags with no tags (property is null or array is empty).
+         */
+        public static final String NEW = "new";
+
+
+        private String calculate;
+
+        @Override public WikipediaTagCategorizerConfig buildFromJsonElement(JsonElement jsonElement) {
+            return PluginConfigHelper.buildFromJson(jsonElement, WikipediaTagCategorizerConfig.class);
+        }
+
+        public String getCalculate() {
+            return calculate;
+        }
+
+        public String setCalculate() {
+            return calculate;
+        }
     }
 }
